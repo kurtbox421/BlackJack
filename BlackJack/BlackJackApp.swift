bad1989d45852f01986eeed863822eb3b487bcdd
Curtis Bollinger <bollingerceb421@gmail.com>
2025-11-30 20:03:39 -0500

    Initial Commit

---
+++ BlackJack/BlackJackApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackJackApp.swift
+//  BlackJack
+//
+//  Created by Curtis Bollinger on 11/30/25.
+//
+
+import SwiftUI
+
+@main
+struct BlackJackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
